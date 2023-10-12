6037b1d93ed8c52117b6dff3089b2829343fefd9
Johannes Koester <johannes.koester@uni-due.de>
2023-10-12 11:28:03 +0200

    use minio play storage for testing

--- tests/tests.py
+++ tests/tests.py
@@ -6,7 +6,7 @@ from snakemake_interface_executor_plugins import ExecutorSettingsBase
 from snakemake_executor_plugin_azure_batch import ExecutorSettings
 
 
-class TestWorkflowsBase(snakemake.common.tests.TestWorkflowsBase):
+class TestWorkflowsBase(snakemake.common.tests.TestWorkflowsMinioPlayStorageBase):
     __test__ = True
 
     def get_executor(self) -> str:
@@ -18,17 +18,3 @@ class TestWorkflowsBase(snakemake.common.tests.TestWorkflowsBase):
             account_url=os.getenv("AZ_BATCH_ACCOUNT_URL"),
             account_key=os.getenv("AZ_BATCH_KEY"),
         )
-
-    def get_default_storage_provider(self) -> Optional[str]:
-        # Return name of default remote provider if required for testing,
-        # otherwise None.
-        return "AzBlob"
-
-    def get_default_storage_prefix(self) -> Optional[str]:
-        # Return default remote prefix if required for testing,
-        # otherwise None.
-        return os.getenv("AZ_BLOB_PREFIX")
-
-    def get_envvars(self):
-        # envvars that will be passed to the batch tasks
-        return ["AZ_BLOB_ACCOUNT_URL", "AZ_BLOB_CREDENTIAL"]
