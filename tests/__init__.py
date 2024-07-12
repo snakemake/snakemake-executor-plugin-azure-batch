7bddf4005123a4b82b9ad17eb8e6d83208d0bc4c
Jake VanCampen <jake.vancampen7@gmail.com>
2024-07-12 11:18:27 -0700

    fix: create pool with mgmt batch client (#10)
    updates batch pool creation to use azure.mgmt.batch.Pool.create instead
    of azure.batch.Pool.add. This is allows the managed identity and
    autoscale settings to be configured at pool creation instead of being
    updated after the fact.
    
    I found that enabling the managed identity after the fact using the
    azure.batch.Pool API does not work as expected. The pool resource must
    be created with the managed identity.
    
    fixes #7

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,7 +4,7 @@ from typing import Optional
 import snakemake.common.tests
 from snakemake_interface_executor_plugins.settings import ExecutorSettingsBase
 
-from snakemake_executor_plugin_azure_batch import ExecutorSettings
+from snakemake_executor_plugin_azure_batch.executor import ExecutorSettings
 
 
 class TestWorkflowsBase(snakemake.common.tests.TestWorkflowsMinioPlayStorageBase):
@@ -15,7 +15,8 @@ class TestWorkflowsBase(snakemake.common.tests.TestWorkflowsMinioPlayStorageBase
         # instantiate ExecutorSettings of this plugin as appropriate
         return ExecutorSettings(
             account_url=os.getenv("SNAKEMAKE_AZURE_BATCH_ACCOUNT_URL"),
-            account_key=os.getenv("SNAKEMAKE_AZURE_BATCH_ACCOUNT_KEY"),
+            subscription_id=os.getenv("SNAKEMAKE_AZURE_BATCH_SUBSCRIPTION_ID"),
+            resource_group_name=os.getenv("SNAKEMAKE_AZURE_BATCH_RESOURCE_GROUP_NAME"),
         )
 
     def get_assume_shared_fs(self) -> bool:
@@ -23,10 +24,9 @@ class TestWorkflowsBase(snakemake.common.tests.TestWorkflowsMinioPlayStorageBase
 
     def get_remote_execution_settings(
         self,
-    ) -> snakemake.settings.RemoteExecutionSettings:
-        return snakemake.settings.RemoteExecutionSettings(
+    ) -> snakemake.settings.types.RemoteExecutionSettings:
+        return snakemake.settings.types.RemoteExecutionSettings(
             seconds_between_status_checks=5,
             envvars=self.get_envvars(),
-            # TODO remove once we have switched to stable snakemake for dev
             container_image="snakemake/snakemake:latest",
         )
