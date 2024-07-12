2ec00caa51f965ac043f3f4118a02f6d93f7f7af
jakevc <jake.vancampen7@gmail.com>
2024-07-11 22:11:45 -0700

    settings.types

--- tests/__init__.py
+++ tests/__init__.py
@@ -25,8 +25,8 @@ class TestWorkflowsBase(snakemake.common.tests.TestWorkflowsMinioPlayStorageBase
 
     def get_remote_execution_settings(
         self,
-    ) -> snakemake.settings.RemoteExecutionSettings:
-        return snakemake.settings.RemoteExecutionSettings(
+    ) -> snakemake.settings.types.RemoteExecutionSettings:
+        return snakemake.settings.types.RemoteExecutionSettings(
             seconds_between_status_checks=5,
             envvars=self.get_envvars(),
             container_image="snakemake/snakemake:latest",
